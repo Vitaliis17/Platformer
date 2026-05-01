a8a3fb3d60f5c7648da3376025e7df3a9f2cff8e
Vitaliis <vitaliy.mixalenok@gmail.com>
2026-05-01 09:19:20 +0300

    Switch Android platform

---
+++ Assets/Scripts/Interacter/Tile/InteractTile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "InteractTile", menuName = "Tile/Interact")]
+public class InteractTile : Tile, IInteractable
+{
+    public void Interact()
+    {
+
+    }
+}
