70e3ec34d640f2f6019eb3b310601eb2c265d6bc
Vitaliis <vitaliy.mixalenok@gmail.com>
2026-07-04 14:35:47 +0300

    .

--- Assets/Scripts/Player/PlayerMovementPresenter.cs
+++ Assets/Scripts/Player/PlayerMovementPresenter.cs
@@ -13,12 +13,14 @@ public class PlayerMovementPresenter : MonoBehaviour
 
     private void Start()
     {
+        const float VerticalOffset = 0.5f;
+
         Observable<Vector2> observable = _movementReader.DirectionChanged.Where(direction => direction.sqrMagnitude != 0f);
 
         observable.Subscribe(direction => _player.MoveHorizontal(direction.x)).AddTo(this);
 
         observable.Where(_ => _ladderMap.IsTriggered.CurrentValue)
-            .Where(direction => Mathf.Abs(direction.y) > 0.3f)
+            .Where(direction => Mathf.Abs(direction.y) > VerticalOffset)
             .Subscribe(direction => _player.MoveVertical(direction.y)).AddTo(this);
 
         _jumpReader.Jumped.Subscribe(_ => _player.Jump()).AddTo(this);
