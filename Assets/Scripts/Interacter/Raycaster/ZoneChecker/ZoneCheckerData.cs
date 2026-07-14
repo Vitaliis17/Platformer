c51b06d7ee0683e8acf19684a5517102bc4e6273
Vitaliis <vitaliy.mixalenok@gmail.com>
2026-07-14 18:21:24 +0300

    Organize folders

--- Assets/Scripts/Interacter/Raycaster/ZoneChecker/ZoneCheckerData.cs
+++ Assets/Scripts/Interacter/Raycaster/ZoneChecker/ZoneCheckerData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = nameof(ZoneCheckerData), menuName = nameof(ZoneCheckerData)]
+[CreateAssetMenu(fileName = nameof(ZoneCheckerData), menuName = nameof(ZoneCheckerData))]
 public class ZoneCheckerData : ScriptableObject
 {
     [SerializeField] private float _radius;
