75af3947f45fa0b26da96ad1f12ad62869f3d541
Vitaliis <vitaliy.mixalenok@gmail.com>
2026-07-19 16:09:39 +0300

    Fix borders

--- Assets/Scripts/Application/ScreenScaler.cs
+++ Assets/Scripts/Application/ScreenScaler.cs
@@ -5,16 +5,17 @@ public class ScreenScaler : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private Vector2 _aspectRatio;
 
-    [SerializeField] private Transform[] _transforms;
+    [SerializeField] private Transform _map;
 
     private void Awake()
     {
         Vector2 newAspectRatio = new(_camera.pixelWidth, _camera.pixelHeight);
 
-        float coefficientX = newAspectRatio.x / _aspectRatio.x * _aspectRatio.y / newAspectRatio.y;
-        float coefficientY = newAspectRatio.y / _aspectRatio.y * _aspectRatio.x / newAspectRatio.x;
+        float oldCoefficient = _aspectRatio.x / _aspectRatio.y;
+        float newCoefficient = newAspectRatio.x / newAspectRatio.y;
 
-        foreach (Transform transform in _transforms)
-            transform.localScale = new(transform.localScale.x * coefficientX, transform.localScale.y * coefficientY);
+        float newScaleX = newCoefficient / oldCoefficient * transform.localScale.x;
+
+        _map.localScale = new(newScaleX, _map.localScale.y);
     }
 }
